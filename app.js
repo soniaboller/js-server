dbe9e66fd0e77aaa7b8fa45ea4d8443b3159cf69
Sonia Boller <soniaboller@Sonias-MacBook-Pro.local>
2016-10-20 15:29:33 -0500

    added cors support

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var routes = require('./controllers/index');
 var users = require('./controllers/users');
 
 var app = express();
+
+//enable cors
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
